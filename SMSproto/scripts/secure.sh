084c6d9b8b81a37bb1815db5b7c5b8ef0bb5afff
Hadrien Croubois <hc@iex.ec>
2018-12-27 16:03:19 +0100

    README

--- SMSproto/scripts/secure.sh
+++ SMSproto/scripts/secure.sh
@@ -5,11 +5,11 @@ curl -H "Content-Type: application/json" -X GET -d '{
 	{
 		"worker": "0x748e091bf16048cb5103E0E10F9D5a8b7fBDd860",
 		"taskid": "0x098f400f58acd32ac4016fe3f95aaf9b3718d3906dd975fe8a65c7648e72954d",
-		"enclave": "0x7943E75A48FEf0376CCDcdda87fea71a4A9730C4",
+		"enclave": "0x3E04a05575731Fbf21d82C72D72a5DD8b20FaF38",
 		"sign":
 		{
-			"r": "0x9662f9516fada7ca4e19263b0206aee86265a3bf9cf91c0622b11d26341ce4e0",
-			"s": "0x3d9a26a9dd3d28e2e04178a8c9af055b8b50faaf5b0913d1d1737cf09bca3a73",
+			"r": "0x80530f73ad3c6de390ea3fcc46ace2fa038cdff69a6de0b25772d9fabe4c1bd1",
+			"s": "0x00c44ee34460f103f71b6a855f15626ccc2214e147ccede5c745b38fe841cee5",
 			"v": 28
 			}
 	}
