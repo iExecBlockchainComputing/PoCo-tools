e8bded9c5fddd17f45a7576aa109a1d04d3a7345
Hadrien Croubois <hc@iex.ec>
2019-04-08 17:31:15 +0200

    derivate aes key length from rsa public key size

--- scripts/encryption/resultEncrypt.sh
+++ scripts/encryption/resultEncrypt.sh
@@ -30,33 +30,43 @@ if [ ! -f "$PUB_KEY" ]; then echo "missing key file '$PUB_KEY'"; exit 255; fi
 ROOT_FOLDER="iexec_out"
 ENC_RESULT_FILE="result.zip.aes"
 ENC_KEY_FILE="encrypted_key"
-TMP_KEY_FILE=".iexec-tee-temporary-key"
+AES_KEY_FILE=".iexec-tee-temporary-key"
 IV_FILE=".iexec-tee-iv-key"
-
-KEY_LENGTH=16 # 16 bytes = 128bits (for 2048 bits RSA keys)
 IV_LENGTH=16
 
-case ${KEY_LENGTH} in
-	16) AES_ALG="-aes-128-cbc";;
-	32) AES_ALG="-aes-256-cbc";;
-	*) exit 1
+PUB_KEY_SIZE=$(openssl rsa -text -noout -pubin -in ${PUB_KEY} | head -n 1 | grep -o '[[:digit:]]*')
+case ${PUB_KEY_SIZE} in
+	2048)
+		AES_KEY_SIZE=16 # 16 bytes = 128bits
+		AES_ALG="-aes-128-cbc"
+		;;
+	4096)
+		AES_KEY_SIZE=32 # 16 bytes = 128bits
+		AES_ALG="-aes-256-cbc"
+		;;
+	*)
+		echo "unsuported public key size"
+		exit 1
+		;;
 esac
 
 ### MAKE ROOT FOLDER IF NOT EXIST
 mkdir -p ${ROOT_FOLDER}
 
 ### GENERATE AES KEY
-KEY=$(openssl rand ${KEY_LENGTH} | tee ${TMP_KEY_FILE} | od -An -tx1 | tr -d ' \n')
-# echo "KEY:" ${KEY}
+AES_KEY=$(openssl rand ${AES_KEY_SIZE} | tee ${AES_KEY_FILE} | od -An -tx1 | tr -d ' \n')
+# echo "AES KEY SIZE:" ${AES_KEY_SIZE}
+# echo "AES KEY:" ${AES_KEY}
 
 ### GENERATE IV
 IV=$(openssl rand ${IV_LENGTH} | tee ${IV_FILE} | od -An -tx1 | tr -d ' \n')
+# echo "IV LENGTH:" ${IV}
 # echo "IV:" ${IV}
 
 ### ENCRYPT RESULT AND AES KEY
 mv ${IV_FILE} ${ROOT_FOLDER}/${ENC_RESULT_FILE}
-openssl enc ${AES_ALG} -K ${KEY} -iv ${IV} -in ${INPUT} >> ${ROOT_FOLDER}/${ENC_RESULT_FILE}
-openssl rsautl -encrypt -oaep -inkey ${PUB_KEY} -pubin -in ${TMP_KEY_FILE} >> ${ROOT_FOLDER}/${ENC_KEY_FILE}
+openssl enc ${AES_ALG} -K ${AES_KEY} -iv ${IV} -in ${INPUT} >> ${ROOT_FOLDER}/${ENC_RESULT_FILE}
+openssl rsautl -encrypt -oaep -inkey ${PUB_KEY} -pubin -in ${AES_KEY_FILE} >> ${ROOT_FOLDER}/${ENC_KEY_FILE}
 
 ### ZIP
 zip -r ${ROOT_FOLDER} ${ROOT_FOLDER}/${ENC_RESULT_FILE} ${ROOT_FOLDER}/${ENC_KEY_FILE}
@@ -65,7 +75,7 @@ zip -r ${ROOT_FOLDER} ${ROOT_FOLDER}/${ENC_RESULT_FILE} ${ROOT_FOLDER}/${ENC_KEY
 rm ${ROOT_FOLDER}/${ENC_RESULT_FILE} ${ROOT_FOLDER}/${ENC_KEY_FILE}
 
 ### SHRED KEY
-shred -u ${TMP_KEY_FILE}
-KEY=""
+shred -u ${AES_KEY_FILE}
+AES_KEY=""
 
 echo "Result succesfully encrypted to '${ROOT_FOLDER}.zip'"
