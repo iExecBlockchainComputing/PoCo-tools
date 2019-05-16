13e6627cffcd92a1276dc188b6763b7678fa5215
Pierre Jeanjacquot <pierre.jeanjacquot@gadz.org>
2019-05-16 18:30:05 +0200

    minors changes for running in iexec SDK (non root user + outputs)

--- scripts/encryption/docker/datasetEncrypt.sh
+++ scripts/encryption/docker/datasetEncrypt.sh
@@ -9,15 +9,17 @@ function encryptFile()
 
 	if [ -e "$in_path/$1" ]
 	then
-		echo "Encrypting '$in_path/$1'"
+		echo "Encrypting $1"
 	else
-		echo "'$in_path/$1' is not a valid file. Skipping."
-		return
+		(>&2 echo "$1 is not a valid file.")
+		exit 1
 	fi
 	openssl rand -out $1.keybin 32
 	openssl enc -aes-256-cbc -pbkdf2 -in $in_path/$1 -out $out_enc_path/$1.enc -kfile $1.keybin
+	echo "Generated key file $1.secret for dateset $1"
 	openssl base64 -e -in $1.keybin -out $out_sec_path/$1.secret
 	shred -u $1.keybin
+	echo "Generated encrypted dataset $1.enc from dateset $1"
 }
 
 while test $# -gt 0; do
